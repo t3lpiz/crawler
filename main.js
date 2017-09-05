d6f92090ace81cffb46da7e318c22de3b2201204
rt3lpiz <rt3lpiz@mail.ru>
2017-09-05 21:27:48 +0300

    first commit

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var Crawler = require('simplecrawler');
+var cheerio = require('cheerio');
 var crawler = new Crawler("http://olx.ro");
 
 
-crawler.on('fetchcomplete', function(queueItem) {
-    console.log(queueItem.url);
-    
+crawler.on('fetchcomplete', function(queueItem, buffer) {
+//    console.log(queueItem.url);
+    var $ = cheerio.load(buffer.toString('utf8'));
+    titlu = $.('div.offer-titlebox h1').text();
+    descriere = $.('div.clr p').text().trim();
+    pret = $.('div.price-label strong').text();
+    console.log("Titlu anunt: %s", titlu);
+    console.log("Pret: %s", pret);
+    console.log("Descriere: %s", descriere);
 });
 
 crawler.interval = 1000;
